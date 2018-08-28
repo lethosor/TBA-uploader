bb084396c624a505da6a2a5dce109f6631076860
lethosor <lethosor@gmail.com>
2018-08-28 01:59:12 -0400

    Add packaging script and versioning info

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
     "flag"
+    "log"
 )
 
+var Version = "unknown"
+
 func main() {
+    log.Printf("Version: %s\n", Version)
     port := flag.Int("port", 8808, "web server port")
     fms_server := flag.String("fms-server", "http://10.0.100.5", "FMS server address (including protocol)")
     no_fms := flag.Bool("no-fms", false, "disable FMS connectivity")
---
+++ release.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+export CGO_ENABLED=0
+
+if git describe --tags --exact-match >/dev/null 2>&1; then
+    version="$(git describe --tags --abbrev=0)"
+else
+    version="$(git describe --tags || true)"
+fi
+test -n "$version" || (echo "Unknown version - missing git tags?" && exit 1)
+echo "Version: $version"
+
+rel_dir="bin/release/$version"
+mkdir -p "$rel_dir"
+echo "Build folder: $rel_dir"
+
+function build_release() {
+    build_dir="$rel_dir/$1-$2"
+    exe_name="TBA-uploader"
+    if [ "$1" = "windows" ]; then
+        exe_name="TBA-uploader.exe"
+    fi
+    echo "[$1/$2] Building $exe_name"
+    mkdir -p "$build_dir"
+    GOOS="$1" GOARCH="$2" go build -o "$build_dir/$exe_name" -ldflags "-X main.Version=$version"
+    zip_name="TBA-uploader-$version-$3.zip"
+    echo "[$1/$2] Generating $zip_name"
+    pushd "$build_dir" >/dev/null
+    zip -r "$zip_name" "$exe_name"
+    mv "$zip_name" ../
+    popd >/dev/null
+    echo "[$1/$2] Done"
+}
+
+build_release windows 386 win32
+build_release windows amd64 win64
+build_release darwin amd64 mac64
+build_release linux amd64 linux64
