6fbb564d7b255bf8839f5de960b2df86d88ff78f
lethosor <lethosor@gmail.com>
2018-08-31 18:44:58 -0400

    Add match/fetch route

--- web.go
+++ web.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "io/ioutil"
     "log"
     "net/http"
+    "strconv"
 
     "github.com/gorilla/mux"
 )
+
+type AllianceSummary struct {
+    Teams []int `json:"teams"`
+    Score int `json:"score"`
+    Rp int `json:"rp"`
+}
+
+type MatchSummary struct {
+    Red AllianceSummary `json:"red"`
+    Blue AllianceSummary `json:"blue"`
+}
 
 func getRequestEventParams(r *http.Request) (*eventParams, bool) {
     if len(r.Header.Get("X-Event")) > 0 && len(r.Header.Get("X-Auth")) > 0 && len(r.Header.Get("X-Secret")) > 0 {
@@ -63,6 +76,43 @@ func apiUploadMatches(w http.ResponseWriter, r *http.Request) {
     apiTBARequest("matches/update", w, r)
 }
 
+func apiFetchMatches(w http.ResponseWriter, r *http.Request) {
+    level, err := strconv.Atoi(r.URL.Query().Get("level"))
+    download_all := (r.URL.Query().Get("all") != "")
+    if err != nil || (level < 1 || level > 3) {
+        w.WriteHeader(http.StatusBadRequest)
+        w.Write([]byte(fmt.Sprintf("invalid level: %d", level)))
+        return
+    }
+    var files []string
+    if download_all {
+        files, err = downloadAllMatches(level, "")
+    } else {
+        files, err = downloadNewMatches(level, "")
+    }
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        w.Write([]byte(fmt.Sprintf("match downloaded failed: %s", err)))
+        return
+    }
+
+    info := make(map[string]MatchSummary)
+    if files != nil {
+        for i := 0; i < len(files); i++ {
+            info[files[i]] = MatchSummary{}
+        }
+    }
+
+    output, err := json.Marshal(info)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        w.Write([]byte(fmt.Sprintf("json encode failed: %s", err)))
+        return
+    }
+
+    w.Write(output)
+}
+
 func RunWebServer(port int, dev bool) {
     r := mux.NewRouter()
     var fs http.FileSystem
@@ -73,6 +123,7 @@ func RunWebServer(port int, dev bool) {
     }
     r.HandleFunc("/js/version.js", jsVersion)
     r.HandleFunc("/api/awards/upload", apiUploadAwards)
+    r.HandleFunc("/api/matches/fetch", apiFetchMatches)
     r.HandleFunc("/api/matches/upload", apiUploadMatches)
     r.PathPrefix("/").Handler(http.FileServer(fs))
     addr := fmt.Sprintf(":%d", port)
