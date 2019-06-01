05662d50fdfd08e7f927125053bbe1fe10e825c2
lethosor <lethosor@gmail.com>
2019-06-01 19:24:25 -0400

    Tweak build/run scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+if [ "$(uname)" = "Darwin" ]; then
+    # need to use system compiler
+    unset CC
+    unset CXX
+fi
+
 run_cmd() {
     echo "=> $@"
     "$@"
--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ run_cmd() {
 }
 
 cd $(dirname "$0")
-./build.sh
+test -n "$build" && ./build.sh
 run_cmd ./bin/TBA-uploader "$@" -data-folder ./fms_data -web-folder ./web
