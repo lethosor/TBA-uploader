5aa30d841546c31a91e812429efc2d781f2e4d39
lethosor <lethosor@gmail.com>
2019-07-02 20:19:12 -0400

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+run_cmd() {
+    echo "=> $@"
+    "$@"
+}
+
+run_cmd go get github.com/jteeuwen/go-bindata/...
+run_cmd go get github.com/elazarl/go-bindata-assetfs/...
+run_cmd go get github.com/gorilla/mux
+run_cmd go get github.com/PuerkitoBio/goquery
