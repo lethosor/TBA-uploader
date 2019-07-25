f9d38fc769fb5a58046d5e3b15ecb31dee767404
lethosor <lethosor@gmail.com>
2019-07-24 21:43:48 -0400

    Switch to just go-bindata

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 
 cd $(dirname "$0")
 run_cmd cp README.md web/
-run_cmd go-bindata-assetfs web/... -o bindata_assetfs.go
+run_cmd go-bindata -fs -prefix web/ -o bindata_assetfs.go web/...
 run_cmd go build -o bin/TBA-uploader
--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 . common.inc.sh
 
-run_cmd go get github.com/jteeuwen/go-bindata/...
-run_cmd go get github.com/elazarl/go-bindata-assetfs/...
-run_cmd go get github.com/gorilla/mux
-run_cmd go get github.com/PuerkitoBio/goquery
+run_cmd go get -u github.com/go-bindata/go-bindata/...
+run_cmd go get -u github.com/gorilla/mux
+run_cmd go get -u github.com/PuerkitoBio/goquery
