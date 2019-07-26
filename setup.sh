07b6eca3f77bf642d5d2c1a8eeab60067f1d04b0
lethosor <lethosor@gmail.com>
2019-07-25 20:54:42 -0400

    Add package.json, webpack config

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@
 run_cmd go get -u github.com/go-bindata/go-bindata/...
 run_cmd go get -u github.com/gorilla/mux
 run_cmd go get -u github.com/PuerkitoBio/goquery
+run_cmd yarn install
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        path.join(__dirname, 'web', 'app.js'),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'web'),
+    },
+};
