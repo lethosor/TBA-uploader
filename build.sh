212cc15d4ff283562d9de939ae25a03174e32dc5
lethosor <lethosor@gmail.com>
2019-08-13 23:27:13 -0400

    Build consts.js before files that depend on it

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 . common.inc.sh
 
 cd $(dirname "$0")
-test -n "$build_js" && run_cmd yarn run --silent build --display errors-only
 run_cmd node consts-gen.js consts.json --output-go consts.go --output-js web/src/consts.js
+test -n "$build_js" && run_cmd yarn run --silent build --display errors-only
 run_cmd cp README.md web/dist/
 run_cmd go-bindata -fs -prefix web/dist/ -o bindata_assetfs.go web/dist/...
 run_cmd go build -o bin/TBA-uploader
