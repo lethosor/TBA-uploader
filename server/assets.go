e925cbafd1cc98953ef5cf2bcf15d994136651b5
lethosor <lethosor@gmail.com>
2023-05-14 17:05:21 -0400

    Fix backend CI while frontend is not built

--- server/assets.go
+++ server/assets.go
@@ -10,7 +10,7 @@ const ASSETS_BASE_PATH = "tmp/assets/"
 //go:generate rm -rf tmp/assets/
 //go:generate mkdir -p tmp/assets
 //go:generate cp -r ../web/build tmp/assets/web
-//go:embed all:tmp/assets/web
+//go:embed all:tmp/assets/web/*
 var fsWebEmbedded embed.FS
 
 var fsWeb fs.FS
