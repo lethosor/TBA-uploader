bb9ad46174ccad7112b0ecc59bcc6fbd18ee337c
lethosor <lethosor@gmail.com>
2023-05-13 23:56:47 -0400

    Add button classes to tailwind safelist

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -4,10 +4,15 @@ const config = {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		// from https://www.skeleton.dev/docs/get-started (manual)
-		require('path').join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
+		require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}'),
+	],
+	safelist: [
+		'variant-filled',
+		'variant-filled-error',
+		'variant-filled-primary',
+		'variant-filled-secondary',
+		'variant-filled-success',
+		'variant-filled-warning',
 	],
 	theme: {
 		extend: {},
