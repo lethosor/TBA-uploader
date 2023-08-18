c52f44dcca1f89c2dc0032cf4ea28cfcbc5ff536
lethosor <lethosor@gmail.com>
2023-08-18 01:00:14 -0400

    Implement initial settings and list route

--- cmd/autoav-helper/main.go
+++ cmd/autoav-helper/main.go
@@ -1,5 +1,118 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"runtime/debug"
+	"strings"
+)
+
+var settings struct {
+	VideoDir   string
+	FilePrefix string
+	FileSuffix string
+}
+
+type FileInfo struct {
+	Name  string
+	Mtime int64
+}
+
 func main() {
-	println("foo")
+	settings.VideoDir = "/tmp/videos"
+	settings.FilePrefix = "FiM Comp"
+	settings.FileSuffix = ".mp4"
+
+	mux := http.NewServeMux()
+	handle := func(method string, path string, handler func(w http.ResponseWriter, r *http.Request)) {
+		mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					log.Printf("Internal error: %v\n%s", err, debug.Stack())
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte(fmt.Sprintf("Internal error: %v", err)))
+				}
+			}()
+
+			log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+			if method != r.Method {
+				w.WriteHeader(http.StatusMethodNotAllowed)
+				w.Write([]byte("method not allowed: " + r.Method))
+				return
+			}
+			handler(w, r)
+		})
+	}
+	handle(http.MethodGet, "/", handleRoot)
+	handle(http.MethodPost, "/save", handleSaveSettings)
+	handle(http.MethodGet, "/api/list", apiList)
+
+	addr := ":8807"
+	log.Printf("listening on %s", addr)
+	http.ListenAndServe(addr, mux)
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "text/html")
+	w.Write([]byte(fmt.Sprintf(`
+		<form action="/save" method="POST">
+			<label>
+				Video folder:
+				<input name="VideoDir" value="%s">
+			</label>
+			<br>
+			<label>
+				Video file prefix:
+				<input name="FilePrefix" value="%s">
+			</label>
+			<br>
+			<label>
+				Video file suffix (extension with "."):
+				<input name="FileSuffix" value="%s">
+			</label>
+			<br><br>
+			<input type="submit" value="Save">
+		</form>
+	`, html.EscapeString(settings.VideoDir), html.EscapeString(settings.FilePrefix), html.EscapeString(settings.FileSuffix))))
+}
+
+func handleSaveSettings(w http.ResponseWriter, r *http.Request) {
+	if val := r.FormValue("VideoDir"); val != "" {
+		settings.VideoDir = val
+	}
+	if val := r.FormValue("FilePrefix"); val != "" {
+		settings.FilePrefix = val
+	}
+	if val := r.FormValue("FileSuffix"); val != "" {
+		settings.FileSuffix = val
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func apiList(w http.ResponseWriter, r *http.Request) {
+	all_files, err := ioutil.ReadDir(settings.VideoDir)
+	if err != nil {
+		panic(err)
+	}
+
+	files := make([]FileInfo, 0)
+	for _, file := range all_files {
+		if file.Mode().IsRegular() && strings.HasPrefix(file.Name(), settings.FilePrefix) && strings.HasSuffix(file.Name(), settings.FileSuffix) {
+			files = append(files, FileInfo{
+				Name:  file.Name(),
+				Mtime: file.ModTime().Unix(),
+			})
+		}
+	}
+
+	out, err := json.Marshal(files)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Write(out)
 }
